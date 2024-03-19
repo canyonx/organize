43439fa2153e5d137319886f357400cfc47ac774
Mattéo <lekokeliko@gmail.com>
2024-03-19 20:17:54 +0100

    works on local

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,4 @@
 import './bootstrap.js';
-// import './vendor/leaflet/leaflet.index.js';
-// import './vendor/@webgeodatavore/photon-geocoder-autocomplete/photon-geocoder-autocomplete.index.js';
-
 
 /*
  * Welcome to your app's main JavaScript file!
@@ -9,16 +6,5 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
- 
-//  js part of bootstrap
-// import './vendor/bootstrap/dist/js/bootstrap.min.js';
 
-// Other Css
-// import './vendor/@webgeodatavore/photon-geocoder-autocomplete/dist/photon-geocoder-autocomplete.min.css';
-// import './vendor/leaflet/dist/leaflet.min.css';
-// import './styles/app.css';
-// import './styles/photon-no-map.css';
-// import './styles/custom.scss';
-
-
-// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+import './photon-geocoder-autocomplete/dist/photon-geocoder-autocomplete.min.js';
