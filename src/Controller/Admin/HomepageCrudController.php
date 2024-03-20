2348d9ce6cd62ac7e33af1741e18ffa00acf4ec1
Mattéo <lekokeliko@gmail.com>
2024-03-21 00:27:48 +0100

    effect header

--- src/Controller/Admin/HomepageCrudController.php
+++ src/Controller/Admin/HomepageCrudController.php
@@ -3,18 +3,26 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Homepage;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 
 class HomepageCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Homepage::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::NEW, Action::DELETE);
     }
 
     public function configureFields(string $pageName): iterable
