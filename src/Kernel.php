1328033a431d07e72ddf76e5a135e39afffe8431
Mattéo <lekokeliko@gmail.com>
2024-03-22 16:25:28 +0100

    correcting depreciations

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,8 +7,9 @@ use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
-class Kernel extends BaseKernel
+class Kernel extends BaseKernel implements CompilerPassInterface
 {
     use MicroKernelTrait;
 
