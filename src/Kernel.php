d41db577678eca49ca248542592b1c0ef1a312fe
Mattéo <lekokeliko@gmail.com>
2024-03-22 16:14:31 +0100

    correcting depreciations

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,12 +2,22 @@
 
 namespace App;
 
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function process(ContainerBuilder $container): void
+    {
+        $container
+            ->getDefinition("doctrine.orm.configuration")
+            ->addMethodCall("setIdentityGenerationPreferences", [[PostgreSQLPlatform::class => ClassMetadata::GENERATOR_TYPE_SEQUENCE]]);
+    }
 
     public function boot(): void
     {
