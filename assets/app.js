48dbc23f1588825389395e62f6f54d3dfd31c173
Mattéo <lekokeliko@gmail.com>
2024-03-24 16:11:52 +0100

    photon css

--- assets/app.js
+++ assets/app.js
@@ -10,9 +10,4 @@ import './bootstrap.js';
 // Bootstrap js 
 import './vendor/bootstrap/dist/js/bootstrap.min.js'; 
 
-// import './photon-geocoder-autocomplete/dist/photon-geocoder-autocomplete.min.js';
-
-// import './vendor/bootstrap/dist/css/bootstrap.min.css'; 
 import './vendor/leaflet/dist/leaflet.min.css';
-// import './photon-geocoder-autocomplete/dist/photon-geocoder-autocomplete.min.css';
-import './styles/photon-no-map.scss';
