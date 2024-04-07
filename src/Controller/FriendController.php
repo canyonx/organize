675b336c309c47aae7a62e57d995c2259458ea81
Mattéo <lekokeliko@gmail.com>
2024-04-07 11:02:49 +0200

    add friends pages

--- src/Controller/FriendController.php
+++ src/Controller/FriendController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Friend;
 use App\Repository\FriendRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/friend')]
+#[Route('/relations')]
 class FriendController extends AbstractController
 {
-    #[Route('/', name: 'app_friend_index', methods: ['GET'])]
-    public function index(FriendRepository $friendRepository): Response
+    #[Route('/suivis', name: 'app_friend_followed', methods: ['GET'])]
+    public function friends(FriendRepository $friendRepository): Response
     {
-        return $this->render('friend/index.html.twig', [
-            'friends' => $friendRepository->findAll(),
+        return $this->render('friend/followed.html.twig', [
+            'friends' => $friendRepository->findBy(['member' => $this->getUser(), 'status' => Friend::FRIEND], []),
+        ]);
+    }
+
+    #[Route('/bloques', name: 'app_friend_bloqued', methods: ['GET'])]
+    public function blocked(FriendRepository $friendRepository): Response
+    {
+        return $this->render('friend/blocked.html.twig', [
+            'friends' => $friendRepository->findBy(['member' => $this->getUser(), 'status' => Friend::BLOCKED], []),
         ]);
     }
 }
