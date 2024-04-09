f19b67311ee3e5311f0e47b0a23b8b9b4c5589f5
Mattéo <lekokeliko@gmail.com>
2024-04-09 15:00:27 +0200

    profile design

--- src/Controller/FriendController.php
+++ src/Controller/FriendController.php
@@ -11,19 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/relations')]
 class FriendController extends AbstractController
 {
-    #[Route('/suivis', name: 'app_friend_followed', methods: ['GET'])]
+    #[Route('/', name: 'app_friend_relation', methods: ['GET'])]
     public function friends(FriendRepository $friendRepository): Response
     {
         return $this->render('friend/followed.html.twig', [
-            'friends' => $friendRepository->findBy(['member' => $this->getUser(), 'status' => Friend::FRIEND], []),
-        ]);
-    }
-
-    #[Route('/bloques', name: 'app_friend_blocked', methods: ['GET'])]
-    public function blocked(FriendRepository $friendRepository): Response
-    {
-        return $this->render('friend/blocked.html.twig', [
-            'friends' => $friendRepository->findBy(['member' => $this->getUser(), 'status' => Friend::BLOCKED], []),
+            'followed' => $friendRepository->findBy(['member' => $this->getUser(), 'status' => Friend::FRIEND], []),
+            'blocked' => $friendRepository->findBy(['member' => $this->getUser(), 'status' => Friend::BLOCKED], []),
         ]);
     }
 }
